632fd0a7427f974ecdd44926d829029dd734be4c
cycoe <cycoe@cycoe.cc>
2017-12-20 22:55:26 +0800

    视始化 gui

--- gui.py
+++ gui.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 from modules.Robber import Robber
 from modules.Logger import Logger
 from modules.MisUtils import MisUtils
 from modules.String import String
-from tkinter import *
 import pyqrcode
-import threading
+import sys
 
 
 #
@@ -33,38 +33,61 @@ import threading
 #                      Buddha Bless, No Bug !
 
 
-robber = Robber()
-
-
-class Gui(object):
+class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
-        self.window = Tk()
+        super(MainWindow, self).__init__()
+        self.windowWidth = 800
+        self.windowHeight = 600
+        self.screenWidth, self.screenHeight = self.get_screen_size()
+        self.x = (self.screenWidth - self.windowWidth) // 2
+        self.y = (self.screenHeight - self.windowHeight) // 2
 
-    def set_property(self):
-        self.window.title('Class Robber')
-        self.window.geometry('800x600')
+        self.centralWidget = QtWidgets.QWidget(self)
+        self.mainLayout = QtWidgets.QHBoxLayout(self.centralWidget)
+        self.controlLayout = QtWidgets.QGridLayout()
+        self.messageLayout = QtWidgets.QGridLayout()
+        self.usernameLabel = QtWidgets.QLabel('学号: ')
+        self.passwordLabel = QtWidgets.QLabel('密码: ')
+        self.usernameLine = QtWidgets.QLineEdit()
+        self.passwordLine = QtWidgets.QLineEdit()
+        self.loginButton = QtWidgets.QPushButton('登陆')
 
-    def add_elements(self):
-        li = ['C', 'python', 'php', 'html', 'SQL', 'java']
-        movie = ['CSS', 'jQuery', 'Bootstrap']
-        listb = Listbox(self.window)  # 创建两个列表组件
-        listb2 = Listbox(self.window)
-        for item in li:  # 第一个小部件插入数据
-            listb.insert(0, item)
+    def set_var(self):
+        pass
 
-        for item in movie:  # 第二个小部件插入数据
-            listb2.insert(0, item)
+    @staticmethod
+    def get_screen_size():
+        screen = QtWidgets.QDesktopWidget().screenGeometry()
+        return screen.width(), screen.height()
 
-        listb.pack()  # 将小部件放置到主窗口中
-        listb2.pack()
-        self.window.mainloop()  # 进入消息循环
+    def setup_ui(self):
+        self.setGeometry(self.x, self.y, self.windowWidth, self.windowHeight)
+        self.setMinimumSize(self.windowWidth, self.windowHeight)
+        self.setWindowTitle('Class Robber')
+        self.setCentralWidget(self.centralWidget)
+        self.setLayout(self.mainLayout)
+
+        self.mainLayout.addLayout(self.controlLayout)
+        self.mainLayout.addLayout(self.messageLayout)
+        self.controlLayout.addWidget(self.usernameLabel, 0, 0, 1, 1)
+        self.controlLayout.addWidget(self.passwordLabel, 1, 0, 1, 1)
+        self.controlLayout.addWidget(self.usernameLine, 0, 1, 1, 1)
+        self.controlLayout.addWidget(self.passwordLine, 1, 1, 1, 1)
+        self.controlLayout.addWidget(self.loginButton, 1, 2, 1, 1)
+        self.usernameLine.setFixedHeight(30)
+        self.passwordLine.setFixedHeight(30)
+        self.usernameLine.setMinimumWidth(200)
+        self.passwordLine.setMinimumWidth(200)
 
 
 def main():
-    gui = Gui()
-    gui.set_property()
-    gui.add_elements()
+    robber = Robber()
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.setup_ui()
+    mainWindow.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
